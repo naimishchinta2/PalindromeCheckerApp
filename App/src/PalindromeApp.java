ea88c9fcdb6a3965da31e4038ebc93e9967ff626
naimishchinta2 <naimishchinta2@gmail.com>
2026-03-10 10:21:55 +0530

    uc2 is comitted

--- App/src/PalindromeApp.java
+++ App/src/PalindromeApp.java
@@ -1,7 +1,26 @@
 public class PalindromeApp {
-    static void main() {
-        System.out.println("welcome tp palindrome");
-        System.out.println("system 1.o version");
-        System.out.println("sysyenm initialsed succesfully");
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Palindrome Checker App");
+
+        // Hardcoded string
+        String word = "madam";
+
+        // Reverse the string
+        String reversed = "";
+
+        for(int i = word.length() - 1; i >= 0; i--) {
+            reversed = reversed + word.charAt(i);
+        }
+
+        // Check palindrome
+        if(word.equals(reversed)) {
+            System.out.println(word + " is a Palindrome");
+        }
+        else {
+            System.out.println(word + " is Not a Palindrome");
+        }
+
     }
 }
